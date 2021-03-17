8387e930b845e21169744c9f57c59962447d670f
dogefromage <seb.sa@eblcom.ch>
2021-03-17 20:21:51 +0100

    a

--- levelList.js
+++ levelList.js
@@ -1,6 +1,6 @@
 
 const text =  
-'<b>Damer</b>:<br>Molestias laudantium exercitationem dolorum quam. Veritatis corrupti consectetur autem. Laudantium molestiae neque soluta dolor nobis eligendi perspiciatis. Nostrum sit deleniti et. Non dolore id ut aperiam.';
+'<b>Damer</b>:<br>Molestias laudantium exercitationem dolorum quam. Veritatis corrupti consectetur autem.';
 
 const levels = 
 [
